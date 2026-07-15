0571aa57086fcab997b45f2c6dcff5e94965d3ff
CHINTAGADA HARSHINI <chintagadaharshini7@gmail.com>
2026-07-15 05:54:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Bitonic Subarray/longest-bitonic-subarray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def bitonic(self, arr):
+        n = len(arr)
+
+        inc = [1] * n
+        dec = [1] * n
+
+        # Increasing part
+        for i in range(1, n):
+            if arr[i] >= arr[i - 1]:
+                inc[i] = inc[i - 1] + 1
+
+        # Decreasing part
+        for i in range(n - 2, -1, -1):
+            if arr[i] >= arr[i + 1]:
+                dec[i] = dec[i + 1] + 1
+
+        ans = 1
+
+        for i in range(n):
+            ans = max(ans, inc[i] + dec[i] - 1)
+
+        return ans
