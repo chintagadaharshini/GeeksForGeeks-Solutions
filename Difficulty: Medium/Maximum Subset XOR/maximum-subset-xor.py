939d17575ffcadfe68507bfedcfabecd169056b3
CHINTAGADA HARSHINI <chintagadaharshini7@gmail.com>
2026-07-30 20:55:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Subset XOR/maximum-subset-xor.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxSubsetXOR(self, arr):
+        n = len(arr)
+        index = 0
+
+        # Process bits from MSB to LSB
+        for bit in range(31, -1, -1):
+
+            # Find the maximum element with current bit set
+            max_index = index
+            max_ele = -1
+
+            for i in range(index, n):
+                if (arr[i] & (1 << bit)) and arr[i] > max_ele:
+                    max_ele = arr[i]
+                    max_index = i
+
+            # No element has this bit set
+            if max_ele == -1:
+                continue
+
+            # Bring pivot element to current index
+            arr[index], arr[max_index] = arr[max_index], arr[index]
+
+            # Remove this bit from all other elements
+            for i in range(n):
+                if i != index and (arr[i] & (1 << bit)):
+                    arr[i] ^= arr[index]
+
+            index += 1
+
+        # Compute maximum XOR
+        ans = 0
+        for x in arr:
+            ans = max(ans, ans ^ x)
+
+        return ans
